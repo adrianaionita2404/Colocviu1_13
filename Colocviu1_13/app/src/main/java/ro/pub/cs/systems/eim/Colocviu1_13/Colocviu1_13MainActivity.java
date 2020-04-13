733c8c785649fc9a28c188eb3cfea6d3eb87a6ba
Adriana Ionita <alyana@192-168-0-149.rdsnet.ro>
2020-04-13 11:07:17 +0300

    [Colocviu1_13] A.1 - B.2 Done A & B

--- Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
+++ Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
@@ -3,12 +3,56 @@ package ro.pub.cs.systems.eim.Colocviu1_13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Colocviu1_13MainActivity extends AppCompatActivity {
+    private Integer countCard = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colocviu1_13_main);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(Constants.CARD_TEXT)) {
+                TextView allTextView = (TextView) findViewById(R.id.text_dir);
+                allTextView.setText(savedInstanceState.getString(Constants.CARD_TEXT));
+            }
+
+            if (savedInstanceState.containsKey(Constants.CARD_CNT)) {
+                countCard = Integer.parseInt(savedInstanceState.getString(Constants.CARD_CNT));
+                Toast.makeText(this, "Restored state: " + countCard + " (card count)", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void concatDir(View view) {
+        TextView allTextView = (TextView) findViewById(R.id.text_dir);
+        Button currButton = (Button) view;
+
+        String newStr = currButton.getText().toString();
+        String allStr = allTextView.getText().toString();
+
+        if (allStr.isEmpty())
+            allStr = allStr + newStr;
+        else
+            allStr = allStr + "," + newStr;
+
+        allTextView.setText(allStr);
+
+        countCard++;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        TextView allTextView = (TextView) findViewById(R.id.text_dir);
+
+        savedInstanceState.putString(Constants.CARD_TEXT, allTextView.getText().toString());
+        savedInstanceState.putString(Constants.CARD_CNT, countCard.toString());
     }
 }
---
+++ Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Constants.java
@@ -0,0 +1,6 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+public interface Constants {
+    final public static String CARD_TEXT = "Cardinal_points";
+    final public static String CARD_CNT = "Cardinal_count";
+}
